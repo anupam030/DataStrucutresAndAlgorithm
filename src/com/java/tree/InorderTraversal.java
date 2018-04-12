df014c4d674915927e121435b44a7c25cc4b9280
Anupam Srivastava <anupam030@gmail.com>
2018-04-12 21:19:54 +0530

    commit

--- src/com/java/tree/InorderTraversal.java
+++ src/com/java/tree/InorderTraversal.java
@@ -2,11 +2,14 @@ package com.java.tree;
 
 import java.util.Stack;
 
+//L N R
+
 public class InorderTraversal {
 
 	public void inorder(Node node) {
 		Stack<Node> stack = new Stack<Node>();
 
+		//find the leftmost leaf node
 		while (node != null) {
 			stack.push(node);
 			node = node.leftchild;
@@ -14,11 +17,12 @@ public class InorderTraversal {
 
 		while (!stack.isEmpty()) {
 			Node temp = stack.pop();
-
+            // print the node
 			System.out.print(temp.data + " ");
 
 			node = temp.rightchild;
-
+            
+			// if right child exist for the node find the leftmost node for it too
 			while (node != null) {
 				stack.push(node);
 				node = node.leftchild;
--- src/com/java/tree/PostOrderTraversal.java
+++ src/com/java/tree/PostOrderTraversal.java
@@ -1,5 +1,50 @@
 package com.java.tree;
 
+import java.util.Stack;
+
+//L R N
 public class PostOrderTraversal {
+
+	public void postOrder(Node root) {
+
+		Stack<Node> stack = new Stack<Node>();
+
+		Node current = root;
+		Node prev = null;
+
+		if (current != null) {
+			stack.push(current);
+		}
+
+		while (!stack.isEmpty()) {
+			current = stack.peek();
+
+			// first case find the leftmost leaf node
+			if (prev == null || prev.leftchild == current || prev.rightchild == current) {
+
+				if (current.leftchild != null)
+					stack.push(current.leftchild);
+				else if (current.rightchild != null)
+					stack.push(current.rightchild);
+				else {
+					stack.pop();
+					System.out.print(current.data + " ");
+				}
+			} // when traversing up from left node to Parent node
+			else if (current.leftchild == prev) {
+				if (current.rightchild != null)
+					stack.push(current.rightchild);
+				else {
+					stack.pop();
+					System.out.print(current.data + " ");
+				}
+			} // when traversing up from right node to parent node
+			else if (current.rightchild == prev) {
+				stack.pop();
+				System.out.print(current.data + " ");
+			}
+			prev = current;
+		}
+	}
 
 }

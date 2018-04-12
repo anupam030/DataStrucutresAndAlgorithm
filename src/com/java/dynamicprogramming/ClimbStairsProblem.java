d82dc2952fd7591725522ce3f91e8810c7cbe552
Anupam Srivastava <fodmv6@PL1INPUN0379PC.net.plm.eds.com>
2018-04-12 11:04:18 +0530

    commit

--- src/com/java/dynamicprogramming/ClimbStairsProblem.java
+++ src/com/java/dynamicprogramming/ClimbStairsProblem.java
@@ -14,10 +14,31 @@ There are two ways: (1, 1) and (2)
 Input: n = 4
 Output: 5
 (1, 1, 1, 1), (1, 1, 2), (2, 1, 1), (1, 2, 1), (2, 2)*/
+
+/*n can be genralized as well*/
 public class ClimbStairsProblem {
 	
 	public static void main(String arg[]){
 		
+		System.out.println(countWays(4,3));
+		
 	}
+	// Java program to count number of ways to reach S't stair when
+	// a person can climb 1, 2, ..m stairs at a time
+
+	public static int countWays(int S,int m) {
+	 
+		int combination[] = new int[S+1];
+		
+		combination[0] = 1;combination[1] = 1;
+		
+		for(int i =1 ;i<=m;i++) {
+			for(int j=2;j>=i && j<=i;j++) {
+				combination[j]+=combination[j-i];
+			}
+		}
+		return combination[S];
+	}
+   
 
 }

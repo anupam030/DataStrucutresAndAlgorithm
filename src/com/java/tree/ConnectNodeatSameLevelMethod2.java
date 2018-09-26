93d88e3666fae5b1597716dd4edacf523ed8ea97
Anupam Srivastava <fodmv6@PL1INPUN0379PC.net.plm.eds.com>
2018-09-26 13:16:30 +0530

    commit

--- src/com/java/tree/ConnectNodeatSameLevelMethod2.java
+++ src/com/java/tree/ConnectNodeatSameLevelMethod2.java
@@ -5,45 +5,39 @@ package com.java.tree;
  * far left as possible 
  */
 public class ConnectNodeatSameLevelMethod2 {
-
-	private static NodeConnect convertFromNodetoNodeConnect(Node node) {
-		NodeConnect nodeConnect = new NodeConnect();
-		nodeConnect.data = node.data;
-		if (node.leftchild != null)
-			nodeConnect.leftChild = convertFromNodetoNodeConnect(node.leftchild);
-		if (node.rightchild != null)
-			nodeConnect.rightChild = convertFromNodetoNodeConnect(node.rightchild);
-
-		return nodeConnect;
-	}
 	
-	
-	public void ConnectNode(NodeConnect node) {
+	public void ConnectNode(NodeConnect node,NodeConnect nextlevelleftMostNode) {
 		
+		//nextlevelleftMostNode will be the nextlevelleftMostNode for the current level
 		//Base case 
 		if(node == null) {
 			return;
 		}
 
-        // set the next pointer for root's left child 
+        // set the next pointer for node's left child 
 		if (node.leftChild != null) 
 			node.leftChild.nextPointer=node.rightChild;
-		// set the next pointer for root's right child
-		if (node.rightChild != null && node.nextPointer!=null) {
-			node.rightChild.nextPointer= node.nextPointer.leftChild;
+		// set the next pointer for node's right child
+		if (node.nextPointer!=null) {
+			/*while setting the next pointer for node's right child if parent next pointer is not null it means level is not changing ,at that moment set the
+			next pointer for node's right child as parent nextPointer leftchild.*/
+			if (node.rightChild != null) 
+			    node.rightChild.nextPointer= node.nextPointer.leftChild;
+		} else {
+			/*while setting the next pointer for node's right child if parent next pointer will be null it means level is changing ,at that moment set the
+			parent next as the leftmost node of the current level.*/
+			node.nextPointer = nextlevelleftMostNode;
 		}
 		
 		//set nextRight for other child in preorder fashion
-		ConnectNode(node.leftChild);
-		ConnectNode(node.rightChild);
+		ConnectNode(node.leftChild,nextlevelleftMostNode.leftChild);
+		ConnectNode(node.rightChild,nextlevelleftMostNode.leftChild);
 	}
 	
 	
 	public void connect(NodeConnect root) {
 		//In case of parent
-		root.nextPointer = null;
-		
-		
-		ConnectNode(root);
+		root.nextPointer = null;		
+		ConnectNode(root,root.leftChild);
 	}
 }
